719984ee0cc609ef1c2d9baf59e80bb628270575
RiveraGustavo <53343128+RiveraGustavo@users.noreply.github.com>
2021-05-16 10:39:11 -0500

    version estable prueba 2 maquinas

--- src/main/java/com/rmi/repository/DataBase.java
+++ src/main/java/com/rmi/repository/DataBase.java
@@ -12,8 +12,19 @@ import java.util.concurrent.Semaphore;
 public class DataBase {
 
     Semaphore sem;
+
     public DataBase(Semaphore sem) {
-       this.sem = sem;
+        //this.sem = sem;
+    }
+
+    public boolean waitFile(String nameFile) throws InterruptedException {
+        File file = new File(nameFile);
+        boolean fileIsNotLocked = file.renameTo(file);
+        while (!fileIsNotLocked) {
+            Thread.sleep(100);
+            fileIsNotLocked = file.renameTo(file);
+        }
+        return true;
     }
 
     public List<Libro> leerFicheroLibro(String arg) {
@@ -23,21 +34,23 @@ public class DataBase {
         List<Libro> libros = new ArrayList<>();
         Libro libroNuevo;
         try {
-            sem.acquire();
-            archivo = new File(arg);
-            fr = new FileReader(archivo);
-            br = new BufferedReader(fr);
-            String linea;
-            String[] textElements;
-            while ((linea = br.readLine()) != null) {
-                textElements = linea.split(",");
-                libroNuevo = new Libro(Integer.valueOf(textElements[0]), textElements[1].toString(),
-                        Integer.valueOf(textElements[2]), Integer.valueOf(textElements[3]), textElements[4].toString(),
-                        textElements[5].toString());
-                libros.add(libroNuevo);
+            //sem.acquire();
+            if (waitFile(arg)) {
+                archivo = new File(arg);
+                fr = new FileReader(archivo);
+                br = new BufferedReader(fr);
+                String linea;
+                String[] textElements;
+                while ((linea = br.readLine()) != null) {
+                    textElements = linea.split(",");
+                    libroNuevo = new Libro(Integer.valueOf(textElements[0]), textElements[1].toString(),
+                            Integer.valueOf(textElements[2]), Integer.valueOf(textElements[3]), textElements[4].toString(),
+                            textElements[5].toString());
+                    libros.add(libroNuevo);
+                }
+                fr.close();
             }
-            fr.close();
-            sem.release();
+            //sem.release();
         } catch (Exception e) {
             System.out.println("Error en DataBase por " + e.getMessage());
         } finally {
@@ -59,28 +72,30 @@ public class DataBase {
         List<Prestamo> prestamos = new ArrayList<>();
         Prestamo prestamo;
         try {
-            archivo = new File(arg);
-            if (archivo.exists()) {
-                fr = new FileReader(archivo);
-                br = new BufferedReader(fr);
-                String linea;
-                String[] textElements;
-                SimpleDateFormat objSDF = new SimpleDateFormat("dd-MM-yyyy");
-                Date dt_1;
-                Date dt_2;
-                while ((linea = br.readLine()) != null ) {
-                    if(linea.equals("")){
-                        break; 
+            if (waitFile(arg)) {
+                archivo = new File(arg);
+                if (archivo.exists()) {
+                    fr = new FileReader(archivo);
+                    br = new BufferedReader(fr);
+                    String linea;
+                    String[] textElements;
+                    SimpleDateFormat objSDF = new SimpleDateFormat("dd-MM-yyyy");
+                    Date dt_1;
+                    Date dt_2;
+                    while ((linea = br.readLine()) != null) {
+                        if (linea.equals("")) {
+                            break;
+                        }
+                        textElements = linea.split(",");
+                        dt_1 = objSDF.parse(textElements[1].toString());
+                        dt_2 = objSDF.parse(textElements[2].toString());
+                        prestamo = new Prestamo(Integer.valueOf(textElements[0]), dt_1, dt_2,
+                                Integer.valueOf(textElements[3]), textElements[4].toString(),
+                                Boolean.parseBoolean(textElements[5]));
+                        prestamos.add(prestamo);
                     }
-                    textElements = linea.split(",");
-                    dt_1 = objSDF.parse(textElements[1].toString());
-                    dt_2 = objSDF.parse(textElements[2].toString());
-                    prestamo = new Prestamo(Integer.valueOf(textElements[0]), dt_1, dt_2,
-                            Integer.valueOf(textElements[3]), textElements[4].toString(),
-                            Boolean.parseBoolean(textElements[5]));
-                    prestamos.add(prestamo);
+                    fr.close();
                 }
-                fr.close();
             }
         } catch (Exception e) {
             System.out.println("Error en DataBase leer fichero prestamo por " + e.getMessage());
@@ -103,42 +118,44 @@ public class DataBase {
         BufferedReader br;
         Boolean modifico = false;
         try {
-            archivo = new File(arg);
-            if (archivo.exists()) {
-                br = new BufferedReader(new FileReader(archivo));
+            if (waitFile(arg)) {
+                archivo = new File(arg);
+                if (archivo.exists()) {
+                    br = new BufferedReader(new FileReader(archivo));
 
-                Date fechaSolicitud = prestamo.getFechaSolicitud();
-                Date fechaFinalizacion = prestamo.getFechaFinalizacion();
+                    Date fechaSolicitud = prestamo.getFechaSolicitud();
+                    Date fechaFinalizacion = prestamo.getFechaFinalizacion();
 
-                int annioDate1 = fechaSolicitud.getYear() + 1900;
-                int annioDate2 = fechaFinalizacion.getYear() + 1900;
-                int mesDate1 = fechaSolicitud.getMonth() + 1;
-                int mesDate2 = fechaFinalizacion.getMonth() + 1;
+                    int annioDate1 = fechaSolicitud.getYear() + 1900;
+                    int annioDate2 = fechaFinalizacion.getYear() + 1900;
+                    int mesDate1 = fechaSolicitud.getMonth() + 1;
+                    int mesDate2 = fechaFinalizacion.getMonth() + 1;
 
-                String fechaInicial = fechaSolicitud.getDate() + "-" + mesDate1 + "-" + annioDate1;
-                String fechaFinal = fechaFinalizacion.getDate() + "-" + mesDate2 + "-" + annioDate2;
+                    String fechaInicial = fechaSolicitud.getDate() + "-" + mesDate1 + "-" + annioDate1;
+                    String fechaFinal = fechaFinalizacion.getDate() + "-" + mesDate2 + "-" + annioDate2;
 
-                String insertar = prestamo.getIdSolicitud() + "," + fechaInicial + "," + fechaFinal + ","
-                        + prestamo.getIdSolicitante() + "," + prestamo.getCodigoLibro() + ","
-                        + prestamo.getFinalizado().toString();
+                    String insertar = prestamo.getIdSolicitud() + "," + fechaInicial + "," + fechaFinal + ","
+                            + prestamo.getIdSolicitante() + "," + prestamo.getCodigoLibro() + ","
+                            + prestamo.getFinalizado().toString();
 
-                String[] textElements;
-                String linea;
-                while ((linea = br.readLine()) != null) {
-                    textElements = linea.split(",");
-                    if (Integer.parseInt(textElements[0]) == prestamo.getIdSolicitud()) {
-                        escribir(fNuevo, insertar);
-                    } else {
-                        escribir(fNuevo, linea);
+                    String[] textElements;
+                    String linea;
+                    while ((linea = br.readLine()) != null) {
+                        textElements = linea.split(",");
+                        if (Integer.parseInt(textElements[0]) == prestamo.getIdSolicitud()) {
+                            escribir(fNuevo, insertar);
+                        } else {
+                            escribir(fNuevo, linea);
+                        }
                     }
-                }
-                br.close();
-                borrar(archivo);
-                fNuevo.renameTo(archivo);
-                modifico = true;
+                    br.close();
+                    borrar(archivo);
+                    fNuevo.renameTo(archivo);
+                    modifico = true;
 
-            } else {
-                throw new Exception("Error en DataBase fichero prestamo por fichero no existe");
+                } else {
+                    throw new Exception("Error en DataBase fichero prestamo por fichero no existe");
+                }
             }
 
         } catch (Exception e) {
@@ -158,31 +175,33 @@ public class DataBase {
         BufferedReader br;
         Boolean modifico = false;
         try {
-            archivo = new File(arg);
-            if (archivo.exists()) {
-                br = new BufferedReader(new FileReader(archivo));
+            if (waitFile(arg)) {
+                archivo = new File(arg);
+                if (archivo.exists()) {
+                    br = new BufferedReader(new FileReader(archivo));
 
-                String insertar = libro.getID() + "," + libro.getCodigo() + "," + libro.getUnidades() + ","
-                        + libro.getUnidadesPrestadas() + ","
-                        + libro.getTitulo() + "," + libro.getAutor();
-                String[] textElements;
-                String linea;
-                while ((linea = br.readLine()) != null) {
-                    textElements = linea.split(",");
-                    if (Integer.parseInt(textElements[0]) == libro.getID()) {
-                        escribir(fNuevo, insertar);
+                    String insertar = libro.getID() + "," + libro.getCodigo() + "," + libro.getUnidades() + ","
+                            + libro.getUnidadesPrestadas() + ","
+                            + libro.getTitulo() + "," + libro.getAutor();
+                    String[] textElements;
+                    String linea;
+                    while ((linea = br.readLine()) != null) {
+                        textElements = linea.split(",");
+                        if (Integer.parseInt(textElements[0]) == libro.getID()) {
+                            escribir(fNuevo, insertar);
 
-                    } else {
-                        escribir(fNuevo, linea);
+                        } else {
+                            escribir(fNuevo, linea);
+                        }
                     }
-                }
-                br.close();
-                borrar(archivo);
-                fNuevo.renameTo(archivo);
-                modifico = true;
+                    br.close();
+                    borrar(archivo);
+                    fNuevo.renameTo(archivo);
+                    modifico = true;
 
-            } else {
-                throw new Exception("Error en DataBase fichero prestamo por fichero no existe");
+                } else {
+                    throw new Exception("Error en DataBase fichero prestamo por fichero no existe");
+                }
             }
 
         } catch (Exception e) {
@@ -229,31 +248,44 @@ public class DataBase {
         BufferedReader br;
         Boolean modifico = false;
         try {
-            archivo = new File(arg);
-            if (archivo.exists()) {
-                br = new BufferedReader(new FileReader(archivo));
+            if (waitFile(arg)) {
+                archivo = new File(arg);
+                if (archivo.exists()) {
+                    br = new BufferedReader(new FileReader(archivo));
 
-                Date fechaSolicitud = prestamo.getFechaSolicitud();
-                Date fechaFinalizacion = prestamo.getFechaFinalizacion();
+                    Date fechaSolicitud = prestamo.getFechaSolicitud();
+                    Date fechaFinalizacion = prestamo.getFechaFinalizacion();
 
-                int annioDate1 = fechaSolicitud.getYear() + 1900;
-                int annioDate2 = fechaFinalizacion.getYear() + 1900;
-                int mesDate1 = fechaSolicitud.getMonth() + 1;
-                int mesDate2 = fechaFinalizacion.getMonth() + 1;
+                    int annioDate1 = fechaSolicitud.getYear() + 1900;
+                    int annioDate2 = fechaFinalizacion.getYear() + 1900;
+                    int mesDate1 = fechaSolicitud.getMonth() + 1;
+                    int mesDate2 = fechaFinalizacion.getMonth() + 1;
 
-                String fechaInicial = fechaSolicitud.getDate() + "-" + mesDate1 + "-" + annioDate1;
-                String fechaFinal = fechaFinalizacion.getDate() + "-" + mesDate2 + "-" + annioDate2;
+                    String fechaInicial = fechaSolicitud.getDate() + "-" + mesDate1 + "-" + annioDate1;
+                    String fechaFinal = fechaFinalizacion.getDate() + "-" + mesDate2 + "-" + annioDate2;
+                    String lastLine = "";
+                    String sCurrentLine;
 
-                String insertar = prestamo.getIdSolicitud() + "," + fechaInicial + "," + fechaFinal + ","
-                        + prestamo.getIdSolicitante() + "," + prestamo.getCodigoLibro() + ","
-                        + prestamo.getFinalizado().toString();
+                    while ((sCurrentLine = br.readLine()) != null) {
+                        lastLine = sCurrentLine;
+                    }
+                    String [] contenLastLine = lastLine.split(",");
+                    int id = 1; 
+                    if(contenLastLine.length > 0){
+                        id = Integer.parseInt(contenLastLine[0])+1; 
+                    }
+                     
+                    String insertar = String.valueOf(id) + "," + fechaInicial + "," + fechaFinal + ","
+                            + prestamo.getIdSolicitante() + "," + prestamo.getCodigoLibro() + ","
+                            + prestamo.getFinalizado().toString();
 
-                escribir(archivo, insertar);
-                br.close();
-                modifico = true;
+                    escribir(archivo, insertar);
+                    br.close();
+                    modifico = true;
 
-            } else {
-                throw new Exception("Error en DataBase fichero prestamo por fichero no existe");
+                } else {
+                    throw new Exception("Error en DataBase fichero prestamo por fichero no existe");
+                }
             }
 
         } catch (Exception e) {
